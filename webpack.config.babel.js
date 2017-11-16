b123b0e382314a5ed5c603169ba55ad4316ad7a0
afeiship <1290657123@qq.com>
2017-11-16 13:15:39 +0800

    issue: remove console code

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,6 +1,4 @@
 import {Config} from './src/config';
-const TARGET = process.env.npm_lifecycle_event;
-
 
 export default  () => {
   return require(`./build/webpack.config.${Config.env}.babel.js`);
