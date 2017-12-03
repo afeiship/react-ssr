0d68efd3bee20f0df1c70d81888d7b5aa5f2e26a
afeiship <1290657123@qq.com>
2017-12-03 20:42:22 +0800

    update css/scss/image

--- build/webpack.common.babel.js
+++ build/webpack.common.babel.js
@@ -1,5 +1,8 @@
 import webpack from 'webpack';
-import {resolve} from 'path';
+import {resolve, join} from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import autoprefixer from 'autoprefixer';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 
 export default {
@@ -10,10 +13,10 @@ export default {
   output: {
     publicPath: '/',
     path: resolve(__dirname, '../public/build'),
-    filename: 'bundle.js',
+    filename: '[name]-[chunkhash:6].bundle.js',
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.json'],
+    extensions: ['.js', '.jsx', '.json', '.scss', '.css'],
   },
   module: {
     // preLoaders: [{
@@ -22,11 +25,46 @@ export default {
     //   exclude: /node_modules/,
     //   include: Dir.src,
     // }],
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
+        test: /\.(js|jsx)$/,
+        use: ['babel-loader'],
         exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader']
+        })
+      },
+      {
+        test: /\.scss$/,
+        loader: 'import-glob-loader',
+        enforce: "pre"
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: 'url-loader',
+        options: {
+          name: 'images/[name]-[hash:4].[ext]',
+          limit: 8192
+        }
+      },
+      {
+        test: /\.(woff|eot|ttf)\??.*$/,
+        loader: 'url-loader',
+        options: {
+          name: 'fonts/[name]-[hash:4].[ext]',
+          limit: 8192
+        }
       }
     ]
   },
@@ -34,15 +72,71 @@ export default {
     'react': 'React',
     'react-dom': 'ReactDOM',
   },
+
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),  // Webpack 2.0 fixed this mispelling
-
-    // Use NoErrorsPlugin for webpack 1.x
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
       },
     }),
-  ],
+    new ExtractTextPlugin('[name]-[hash].css'),
+    new webpack.ProvidePlugin({
+      nx: 'next-js-core2',
+      autobind: 'autobind-decorator',
+      mixin: 'mixin-decorator',
+    }),
+    new webpack.DllReferencePlugin({
+      manifest: resolve(__dirname, '../dist/vendors/manifest.json')
+    }),
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, '../public/index.html'),
+      title: 'Hot Module Replacement'
+    }),
+    // build optimization plugins
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor-[hash:6].min.js',
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: {
+          plugins: [
+            autoprefixer({
+              browsers: [
+                'last 2 version',
+                'Explorer >= 10',
+                'Android >= 4'
+              ]
+            })
+          ]
+        },
+        img: {
+          gifsicle: {
+            interlaced: false
+          },
+          mozjpeg: {
+            progressive: true,
+            arithmetic: false
+          },
+          optipng: false, // disabled
+          pngquant: {
+            floyd: 0.5,
+            speed: 2
+          },
+          svgo: {
+            plugins: [
+              {
+                removeTitle: true
+              },
+              {
+                convertPathData: false
+              }
+            ]
+          }
+        }
+      }
+    }),
+  ]
 }
